86a4a4f606a7c90c36f157ef611f12751d52d71a
Masaya ARAI <masaya0708@gmail.com>
2020-05-27 01:28:17 +0900

    create Internet Gateway

--- modules/combase/vpc.tf
+++ modules/combase/vpc.tf
@@ -1,5 +1,6 @@
 locals {
   vpc_name = "${var.resource_id}-vpc"
+  igw_name = "${var.resource_id}-igw"
 }
 
 resource "aws_vpc" "main" {
@@ -9,5 +10,13 @@ resource "aws_vpc" "main" {
 
   tags = {
     "Name" = local.vpc_name
+  }
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    "Name" = local.igw_name
   }
 }
