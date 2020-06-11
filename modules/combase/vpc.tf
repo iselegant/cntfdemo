f3b390d6e80ac8014436462e533d832ea02c6203
Masaya ARAI <masaya0708@gmail.com>
2020-06-11 13:18:57 +0900

    add internet path to management route table

--- modules/combase/vpc.tf
+++ modules/combase/vpc.tf
@@ -113,6 +113,11 @@ resource "aws_route_table" "app" {
 resource "aws_route_table" "management" {
   vpc_id = aws_vpc.main.id
 
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
   tags = {
     "Name" = "${local.route_prefix}-management"
   }
