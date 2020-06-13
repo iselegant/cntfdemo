a0e724bfe66439985d717e7100ad0c9b0c803aaf
Masaya ARAI <masaya0708@gmail.com>
2020-06-13 12:33:52 +0900

    fix ingress security group

--- modules/combase/security-group.tf
+++ modules/combase/security-group.tf
@@ -13,6 +13,13 @@ resource "aws_security_group" "ingress" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   tags = {
     "Name" = "${local.sg_pfx}-ingress"
   }
